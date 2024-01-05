dfe970128b3ae57e756ad3f02f7c4c98996a5200
xanadu314 <37304315+xanadu314@users.noreply.github.com>
2024-01-05 15:47:15 +0800

    Create 01_data_preprocessing.sh

---
+++ Pipeline/01_data_preprocessing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##QC step1: filter low quality sequecne and remove adapters by fastp
+while read id
+do
+    fastp -i ${id}_1.fastq.gz -I ${id}_2.fastq.gz -o ${id}_1.clean.fastq.gz -O ${id}_2.clean.fastq.gz -j ${id}.fastp.josn -h ${id}.fastp.html
+done<id.txt
+
+##QC step2: filter host sequence by bowtie2
+###index of reference genome
+bowtie2-build reference.fna reference.db
+while read id
+do
+    bowtie2 --very-sensitive-local -x ~/genome/reference.db -p 2 -1 ${id}_1.fastq.clean.gz -2 ${id}_2.fastq.clean.gz  --un-conc-gz ${id}_clean2.gz >/dev/null 
+done<id.txt
+
+## QC step3: Generate Reports
+while read id
+do
+    fastp -i ${id}_clean2.1.gz -I ${id}clena2.2.gz -o ${id}_1.clean2.fastq.gz -O ${id}_2.clean2.fastq.gz -j ${id}.fastp_final.josn -h ${id}.fastp_final.html
+done<id.txt
